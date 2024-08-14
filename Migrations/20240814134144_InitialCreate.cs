4a76ed968bece95de7b97550d959685882f15db3
Beyza Nur Köşeli <bkoseli20@ku.edu.tr>
2024-08-15 00:43:58 +0300

    I Added success message for registration and updated the Register.cshtml file

---
+++ Migrations/20240814134144_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UniversiteOgrenciYonetimSistemi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ogrenciler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ad = table.Column<string>(type: "text", nullable: false),
+                    Soyad = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Sifre = table.Column<string>(type: "text", nullable: false),
+                    Universite = table.Column<string>(type: "text", nullable: true),
+                    Bolum = table.Column<string>(type: "text", nullable: true),
+                    Sinif = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogrenciler", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ogrenciler");
+        }
+    }
+}
